965f1a42e8083ca849cf500edb431dbc138a0c59
Courtney Mae Phillips <courtney.mae.phillips@gmail.com>
2015-09-20 17:24:57 -0700

    add rental-tile component.

--- config/environment.js
+++ config/environment.js
@@ -6,7 +6,7 @@ module.exports = function(environment) {
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
-    firebase: 'https://super-rentals-courtney.firebaseio.com',
+    firebase: 'https://courtney-super-rentals.firebaseio.com',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
